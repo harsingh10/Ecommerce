98f23a0b9e7511b506c618317714e9f837fd9220
Hardeep Singh <harsingh8769@gmail.com>
2021-05-14 17:44:14 +0530

    Initial Server and database setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//connect to db
+connectDB();
+
+app.get("/", (req,res)=>{res.send("API is running")});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`server is up and running on PORT ${PORT}`)
+});
