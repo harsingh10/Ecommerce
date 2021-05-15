c643f2fc5376f92fc2b3a31dbae77ce9b8737d6d
AbhilashaYdav1305 <AbhilashaYdav1305>
2021-05-15 18:51:08 +0530

    added signUp route & Schema

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const Authenticate = require("./src/routes/Authenticate");
 const app = express();
+
 
 //connect to db
 connectDB();
 
-app.get("/", (req, res) => { res.send("API is running") });
-console.log("change")
+//MiddleWares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+//All the major Routes listed
+app.use("/api/productDemo/auth", Authenticate);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
